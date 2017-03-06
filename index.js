90ee89c2ba366b9185dfad01386d7c929b65914f
Blake Guilloud <fbguillo@gmail.com>
2017-03-05 19:18:22 -0500

    added package

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 #!/usr/bin/env node
 
 const commander = require('commander');
-const nihilo = require('./nihilo');
+const nihilo = require('./src');
 
 nihilo.createProject('test');
 
 commander.version('0.1.5');
 commander.command('nihilo [value]')
   .description('Create a Nihilo Project')
-  .action((name) => {
-    nihilo.createProject(name);
+  .action((projectName) => {
+    nihilo.createProject(projectName);
   });
 
 commander.parse(process.argv);
